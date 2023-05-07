3563efbd14daa79b1724a9bc08301e6320081bec
calc <geraldine.ibieta13@gmail.com>
2023-05-06 23:17:38 -0400

    Creacion del proyecto

---
+++ src/main/kotlin/com/example/Proy_msreviews/ProyMsReviewsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Proy_msreviews
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyMsReviewsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyMsReviewsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Proy_msreviews/ProyMsReviewsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Proy_msreviews
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyMsReviewsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
